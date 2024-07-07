d550d7439ba305d2447c0dbec5556b903b6b4e37
hoangtrong <hoangcongtrong054@gmail.com>
2024-07-07 15:27:03 +0700

    Build fiunction setPartialMessage and modified server app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { fetchEventSource } from '@microsoft/fetch-event-source';
+
+interface Message {
+  message: string;
+  isUser: boolean;
+  sources?: string[];
+}
 
 function App() {
+  const [inputValue, setInputValue] = useState("")
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const setPartialMessage = (chunk: string, sources: string[] = []) => {
+    setMessages(prevMessages => {
+      let lastMessage = prevMessages[prevMessages.length - 1];
+      if (prevMessages.length == 0 || !lastMessage.isUser) {
+        return [...prevMessages.slice(0, -1), {
+          message: lastMessage.message + chunk,
+          isUser: false,
+          sources: lastMessage.sources ? [...lastMessage.sources, ...sources] : sources
+        }];
+      }
+      return [...prevMessages, { message: chunk, isUser: false, sources }];
+    })
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
